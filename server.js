3e1b0937c0be8d51f52806d8d4278de470303d75
Martynas Sileika <sileika85@gmail.com>
2021-07-28 09:19:52 -0700

    connected to mondodb + server is running

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // MiddleWare
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-app.listen(3000);
+// prisijungimas prie duomenu bazes
+mongoose
+  .connect(process.env.MONGO_CONN_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("Conneced Mongo database");
+  })
+  .catch((err) => console.error(err.message));
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
