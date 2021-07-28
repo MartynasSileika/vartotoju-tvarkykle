f133d89ff44c4c883bc35352e67ee7574ef74fc2
Martynas Sileika <sileika85@gmail.com>
2021-07-28 10:08:24 -0700

    binded all input

--- client/src/components/MyForm.jsx
+++ client/src/components/MyForm.jsx
@@ -2,18 +2,31 @@ import React, { Component } from "react";
 
 class MyForm extends Component {
   state = {
-    name: "",
+    name: "Paulius",
     age: "",
     email: "",
     password: "",
   };
+
+  handleSubmitLocal = (e) => {
+    e.preventDefault();
+    console.log("stop right there");
+  };
+
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { state: s } = this;
     return (
       <div className="w-50">
-        <h2>Create new place</h2>
-        <form>
+        <h2>Create new User</h2>
+        <form onSubmit={this.handleSubmitLocal} autoComplete="off">
           <div className="form-group">
             <input
+              value={s.name}
+              onChange={this.handleInput}
               type="text"
               className="form-control"
               name="name"
@@ -22,7 +35,9 @@ class MyForm extends Component {
           </div>
           <div className="form-group">
             <input
-              type="text"
+              value={s.age}
+              onChange={this.handleInput}
+              type="number"
               className="form-control"
               name="age"
               placeholder="Age"
@@ -30,6 +45,8 @@ class MyForm extends Component {
           </div>
           <div className="form-group">
             <input
+              value={s.email}
+              onChange={this.handleInput}
               type="text"
               className="form-control"
               name="email"
@@ -38,12 +55,15 @@ class MyForm extends Component {
           </div>
           <div className="form-group">
             <input
+              value={s.password}
+              onChange={this.handleInput}
               type="text"
               className="form-control"
               name="password"
               placeholder="password"
             />
           </div>
+          <button className="btn btn-primary my-4">Create</button>
         </form>
       </div>
     );
